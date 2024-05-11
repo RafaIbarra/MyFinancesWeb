de2e24fefebe611478a6be1cb44d2192d293dfa3
RafaIbarra <blasrafael1986@gmail.com>
2024-05-10 20:35:49 -0400

    correccion en configuracion de BASE URLs

--- src/peticiones/baseurls.jsx
+++ src/peticiones/baseurls.jsx
@@ -1,5 +1,5 @@
-const APIBASE='http://192.168.1.103:8000/api'
+//const APIBASE='http://192.168.1.103:8000/api'
 //const APIBASE='http://127.0.0.1:8000/api'
 
-//const APIBASE='https://myfinancesbackend.onrender.com/api'
+const APIBASE='https://myfinancesbackend.onrender.com/api'
 export default APIBASE
