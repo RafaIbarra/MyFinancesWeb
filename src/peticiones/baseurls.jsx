29100a30725024a6f7d2817e1611c99049816584
RafaIbarra <blasrafael1986@gmail.com>
2024-06-04 00:59:22 -0400

    config puertos 2

--- src/peticiones/baseurls.jsx
+++ src/peticiones/baseurls.jsx
@@ -1,5 +1,5 @@
-const APIBASE='http://192.168.0.103/api'
-//const APIBASE='https://181.127.188.23:8000/api'
+//const APIBASE='http://192.168.0.103:8000/api'
+const APIBASE='https://181.127.188.23/api'
 //const APIBASE='http://127.0.0.1:8000/api'
 
 //const APIBASE='https://myfinancesbackend.onrender.com/api'
