265841c09864727f5c7c608ad8511089412c4ada
RafaIbarra <blasrafael1986@gmail.com>
2024-06-03 20:16:45 -0400

    cambio ip publica

--- src/peticiones/baseurls.jsx
+++ src/peticiones/baseurls.jsx
@@ -1,5 +1,5 @@
 // const APIBASE='http://192.168.1.103:8000/api'
-const APIBASE='https://181.127.188.23:8000/api'
+const APIBASE='http://181.127.188.23:8000/api'
 //const APIBASE='http://127.0.0.1:8000/api'
 
 //const APIBASE='https://myfinancesbackend.onrender.com/api'
