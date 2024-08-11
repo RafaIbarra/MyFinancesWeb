30418be949969775ad861e51b63416e227c0a5b6
RafaIbarra <blasrafael1986@gmail.com>
2024-08-11 13:36:59 -0400

    redireccionando prueba 3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://181.127.188.23:8443',
+        changeOrigin: true,
+        secure: false, // Desactiva la verificación SSL
+      },
+    },
+  },
+});
